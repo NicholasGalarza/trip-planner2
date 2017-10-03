fab50ef8b984e9cbffe3708474780daae87695cf
NicholasGalarza <nicogala93@gmail.com>
2017-10-03 17:21:48 -0400

    initial commit

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 const mapboxgl = require("mapbox-gl");
 const buildMarker = require("./marker.js");
 
-mapboxgl.accessToken = "YOUR API TOKEN HERE";
+mapboxgl.accessToken = "pk.eyJ1IjoicGluZ3dpbnpsb3R5IiwiYSI6ImNqOGJ0NGF4ejAxcnQyd29razNnM29ncDYifQ.9hm_D0gtkCHlJSEm6rkomg";
 
 const fullstackCoords = [-74.009, 40.705] // NY
 // const fullstackCoords = [-87.6320523, 41.8881084] // CHI
@@ -10,7 +10,7 @@ const map = new mapboxgl.Map({
   container: "map",
   center: fullstackCoords, // FullStack coordinates
   zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+  style: "mapbox://styles/mapbox/light-v9" // mapbox has lots of different map styles available.
 });
 
 const marker = buildMarker("activities", fullstackCoords);
